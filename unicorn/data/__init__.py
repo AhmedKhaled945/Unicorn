a94552c0d68d8fa3ba7fee901ee2c23cece6bb4e
AhmedKhaled945 <a.khaled.taha94@gmail.com>
2022-07-27 13:07:14 +0200

    Update __init__.py

--- unicorn/data/__init__.py
+++ unicorn/data/__init__.py
@@ -5,5 +5,5 @@
 from .data_augment import TrainTransform, ValTransform, TrainTransform_local, TrainTransform_omni, TrainTransform_Ins, TrainTransform_4tasks
 from .data_prefetcher import *
 from .dataloading import DataLoader, get_unicorn_datadir, worker_init_reset_seed
-# from .datasets import *
+from .datasets import *
 from .samplers import InfiniteSampler, YoloBatchSampler
